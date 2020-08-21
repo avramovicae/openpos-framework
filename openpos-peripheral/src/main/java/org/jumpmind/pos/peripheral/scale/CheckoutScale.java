2dac3de60d5b8e83e2764f10195378099db77c65
Mark Michalek <mmichalek@jumpmind.com>
2020-08-21 12:26:45 -0400

    wip

--- openpos-peripheral/src/main/java/org/jumpmind/pos/peripheral/scale/CheckoutScale.java
+++ openpos-peripheral/src/main/java/org/jumpmind/pos/peripheral/scale/CheckoutScale.java
@@ -1,10 +1,12 @@
-package org.jumpmind.pos.peripheral.poledisplay;
+package org.jumpmind.pos.peripheral.scale;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.jumpmind.pos.peripheral.PeripheralException;
 import org.jumpmind.pos.print.IConnectionFactory;
 import org.jumpmind.pos.print.PeripheralConnection;
+import org.jumpmind.pos.print.PrintException;
+import org.jumpmind.pos.print.UsbConnectionFactory;
 import org.jumpmind.pos.util.ClassUtils;
 import org.jumpmind.pos.util.ReflectionException;
 import org.jumpmind.pos.util.status.IStatusManager;
@@ -13,26 +15,21 @@ import org.jumpmind.pos.util.status.Status;
 import org.jumpmind.pos.util.status.StatusReport;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Slf4j
 @Component
-public class PoleDisplay implements IStatusReporter {
+public class CheckoutScale implements IStatusReporter {
 
     public final static String STATUS_NAME = "DEVICE.POLE_DISPLAY";
     public final static String STATUS_ICON = "pole-display";
-
-    final static byte CLEAR_DISPLAY = 0x0C; // NCR
 
     Map<String, Object> settings;
     PeripheralConnection peripheralConnection;
     IConnectionFactory connectionFactory;
 
     IStatusManager statusManager;
-
-    public PoleDisplay() {
-
-    }
 
     public void open(Map<String,Object> settings) {
         this.settings = settings;
@@ -48,44 +45,47 @@ public class PoleDisplay implements IStatusReporter {
             }
             throw new PeripheralException("Failed to create the connection factory for " + getClass().getName(), ex);
         }
-        log.info("Opening pole display with settings: " + this.settings);
+        log.info("Opening checkout scale with settings: " + this.settings);
+
         this.peripheralConnection = connectionFactory.open(this.settings);
-        showText("Starting");
-        log.info("Pole display appears to be successfully opened.");
-    }
+//        showText("Starting");
+        log.info("Checkout scale appears to be successfully opened.");
 
-    public void close() {
-        this.connectionFactory.close(peripheralConnection);
-    }
-
-    public void showText(String text) {
         try {
-            if (peripheralConnection == null || peripheralConnection.getOut() == null) {
-                log.warn("Not connected to pole display to show text: " + text);
-                this.peripheralConnection = connectionFactory.open(this.settings);
+            while (true) {
+                this.peripheralConnection.getOut().write((byte)'W');
+                Thread.sleep(200);
+
+                int lastByte = -1;
+
+                while ((lastByte = this.peripheralConnection.getIn().read()) != -1) {
+                    System.out.println(lastByte);
+                }
+
+                Thread.sleep(2000);
+
             }
-            peripheralConnection.getOut().write(CLEAR_DISPLAY);
-            peripheralConnection.getOut().write(text.getBytes());
         } catch (Exception ex) {
-            if (statusManager != null) {
-                statusManager.reportStatus(new StatusReport(STATUS_NAME, STATUS_ICON, Status.Error, ex.getMessage()));
-            }
-            log.warn("Failed to write text to the PoleDisplay \"" + text + "\"", ex);
+            ex.printStackTrace();
         }
+
     }
+
 
     @Override
     public StatusReport getStatus(IStatusManager statusManager) {
-        this.statusManager = statusManager;
+        StatusReport statusReport = new StatusReport();
+        statusReport.setStatus(Status.Disabled);
+        return statusReport;
+    }
 
-        Status status = (peripheralConnection != null && peripheralConnection.getOut() != null)
-                ? Status.Online : Status.Offline;
+    public static void main(String[] args) {
+        CheckoutScale scale = new CheckoutScale();
+        Map<String, Object> settings = new HashMap<>();
 
-        if (this.settings == null) {
-            status = Status.Disabled;
-        }
-
-        StatusReport report = new StatusReport(STATUS_NAME, STATUS_ICON, status);
-        return report;
+        settings.put("connectionClass", UsbConnectionFactory.class.getName());
+        settings.put("usbVendorId", 0x0eb8);
+        settings.put("usbDeviceId", "*");
+        scale.open(settings);
     }
 }
